fd2c056563dce744cb11df1d0244213562e5c000
Ayo <ayomideyissa@gmail.com>
2022-08-21 01:32:28 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-yarn docs:build
+npm run docs:build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
@@ -14,19 +14,12 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:FriendlyUser/ENGRYear4BNotes.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
-
